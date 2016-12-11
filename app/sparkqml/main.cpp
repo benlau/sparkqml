0b8897599df81504ced5f53461a1d78063b08cd4
Ben Lau <xbenlau@gmail.com>
2016-12-11 10:37:37 +0800

    main.cpp: Show backtrace on crash.

--- app/sparkqml/main.cpp
+++ app/sparkqml/main.cpp
@@ -3,8 +3,31 @@
 #include <QQmlApplicationEngine>
 #include "appview.h"
 
+#if defined(Q_OS_MAC) || defined(Q_OS_LINUX)
+#include <execinfo.h>
+#include <signal.h>
+#include <unistd.h>
+
+void handleBacktrace(int sig) {
+    void *array[100];
+    size_t size;
+
+    // get void*'s for all entries on the stack
+    size = backtrace(array, 100);
+
+    // print out all the frames to stderr
+    fprintf(stderr, "Error: signal %d:\n", sig);
+    backtrace_symbols_fd(array, size, STDERR_FILENO);
+    exit(1);
+}
+#endif
+
 int main(int argc, char *argv[])
 {
+#if defined(Q_OS_MAC) || defined(Q_OS_LINUX)
+    signal(SIGSEGV, handleBacktrace);
+#endif
+
     QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
